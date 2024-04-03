8614a38d768cedff6fec4f5014e257045c2c35b4
nithyanandaraosl <nithyanandarao@changejar.in>
2024-04-03 18:12:29 +0530

    6th commit(The controller only contains the end points and returns,rest is put in services)

--- src/main/java/in/nithya/springbootmongodb/controller/TodoController.java
+++ src/main/java/in/nithya/springbootmongodb/controller/TodoController.java
@@ -1,9 +1,10 @@
+//Contains all the endpoints for the transactional api
 package in.nithya.springbootmongodb.controller;
-
 import in.nithya.springbootmongodb.model.TodoDTO;
 import in.nithya.springbootmongodb.repository.TodoRepository;
 import in.nithya.springbootmongodb.service.CurrencyConversionService;
 import in.nithya.springbootmongodb.service.PaymentService;
+import in.nithya.springbootmongodb.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,84 +20,44 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 public class TodoController {
 
 
-
-    private final TodoRepository todoRepo;
-
-
-    private final CurrencyConversionService conversionService;
-
+    private final TodoService todoService;
 
     private final PaymentService paymentService;
     @Autowired
-    public TodoController(TodoRepository todoRepo, CurrencyConversionService conversionService, PaymentService paymentService) {
-        this.todoRepo = todoRepo;
-        this.conversionService = conversionService;
+    public TodoController(TodoRepository todoRepo, CurrencyConversionService conversionService, TodoService todoService, PaymentService paymentService) {
+
+        this.todoService = todoService;
         this.paymentService = paymentService;
     }
 
 
     @GetMapping("/todos")
     public ResponseEntity<?>getAllTodos(){
-        List< TodoDTO> todos=todoRepo.findAll();
-        if(todos.size()>0){
-            return new ResponseEntity<List<TodoDTO>>(todos, HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>("No todos available",HttpStatus.NOT_FOUND);
-        }
+        return todoService.getAllTodos();
     }
 
     @PostMapping("/todosPost")
     public ResponseEntity<?>createTodo(@RequestBody TodoDTO todo){
-        try {
-            double amountInINR = conversionService.convertToINR(todo.getPaymentMadeIn(), Double.parseDouble(todo.getPayment()));
-            todo.setPayment(String.valueOf(amountInINR));
-            todo.setCreatedAt(new Date(System.currentTimeMillis()));
-            todoRepo.save(todo);
-            return new ResponseEntity<TodoDTO>(todo,HttpStatus.OK);
-        }catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return todoService.createTodo(todo);
     }
 
     @GetMapping("/todos/{id}")
     public ResponseEntity<?> getSingleTodo(@PathVariable("id")String id){
-        Optional<TodoDTO> todoOptional=todoRepo.findById(id);
-        if(todoOptional.isPresent()){
-            return new ResponseEntity<>(todoOptional.get(),HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>("Todo not found with id "+id,HttpStatus.NOT_FOUND);
-        }
+        return todoService.getSingleTodoById(id);
     }
 
     @PutMapping("/todosu/{id}")
     public ResponseEntity<?> updateById(@PathVariable("id")String id,@RequestBody TodoDTO todo){
-        Optional<TodoDTO> todoOptional=todoRepo.findById(id);
-        if(todoOptional.isPresent()){
-            TodoDTO todoToSave=todoOptional.get();
-            todoToSave.setCompleted(todo.getCompleted() != null ? todo.getCompleted(): todoToSave.getCompleted());
-            todoToSave.setPayment(todo.getCompleted()!=null? todo.getPayment():todoToSave.getPayment());
-            todoToSave.setDescription(todo.getDescription()!=null? todo.getDescription():todoToSave.getDescription());
-            todoToSave.setUsername(todo.getUsername()!=null? todo.getUsername():todoToSave.getUsername());
-            todoToSave.setUpdatedAt(new Date(System.currentTimeMillis()));
-            todoRepo.save(todoToSave);
-            return new ResponseEntity<>(todoToSave,HttpStatus.OK);
+        return todoService.updateTodoById(id, todo);
 
-        }else{
-            return new ResponseEntity<>("Todo not found with id "+id,HttpStatus.NOT_FOUND);
-        }
     }
 
     @DeleteMapping("/todosd/{id}")
     public ResponseEntity<?> deleteById(@PathVariable("id") String id){
-        try{
-            todoRepo.deleteById(id);
-            return new ResponseEntity<>("Successfully deleted with id "+id,HttpStatus.OK);
-        } catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(), NOT_FOUND);
-        }
+        return todoService.deleteTodoById(id);
     }
 
-    @GetMapping("/totalPayments")
+    @GetMapping("/totalPayments")//returns the total sum of the all amounts in the database
     public ResponseEntity<Double> getTotalPayments() {
         double totalPayments = paymentService.getTotalPayments();
         return new ResponseEntity<>(totalPayments, HttpStatus.OK);
---
+++ src/main/java/in/nithya/springbootmongodb/service/TodoService.java
@@ -0,0 +1,82 @@
+package in.nithya.springbootmongodb.service;
+
+import in.nithya.springbootmongodb.model.TodoDTO;
+import in.nithya.springbootmongodb.repository.TodoRepository;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+@Service
+public class TodoService {
+
+    private final TodoRepository todoRepo;
+
+
+    private final CurrencyConversionService currencyConversionService;
+    @Autowired
+    public TodoService(TodoRepository todoRepo, CurrencyConversionService currencyConversionService) {
+        this.todoRepo = todoRepo;
+        this.currencyConversionService = currencyConversionService;
+    }
+
+    public ResponseEntity<?> updateTodoById(String id, TodoDTO todo) {
+        Optional<TodoDTO> todoOptional = todoRepo.findById(id);
+        if (todoOptional.isPresent()) {
+            TodoDTO todoToSave = todoOptional.get();
+            todoToSave.setCompleted(todo.getCompleted() != null ? todo.getCompleted() : todoToSave.getCompleted());
+            todoToSave.setPayment(todo.getCompleted() != null ? todo.getPayment() : todoToSave.getPayment());
+            todoToSave.setDescription(todo.getDescription() != null ? todo.getDescription() : todoToSave.getDescription());
+            todoToSave.setUsername(todo.getUsername() != null ? todo.getUsername() : todoToSave.getUsername());
+            todoToSave.setUpdatedAt(new Date(System.currentTimeMillis()));
+            todoRepo.save(todoToSave);
+            return new ResponseEntity<>(todoToSave, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Todo not found with id " + id, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public ResponseEntity<?> getAllTodos() {
+        List<TodoDTO> todos = todoRepo.findAll();
+        if (!todos.isEmpty()) {
+            return new ResponseEntity<>(todos, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("No entries available", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public ResponseEntity<?> createTodo(TodoDTO todo) {
+        try {
+            double amountInINR = currencyConversionService.convertToINR(todo.getPaymentMadeIn(), Double.parseDouble(todo.getPayment()));
+            todo.setPayment(String.valueOf(amountInINR));
+            todo.setCreatedAt(new Date(System.currentTimeMillis()));
+            todoRepo.save(todo);
+            return new ResponseEntity<>(todo, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<?> getSingleTodoById(String id) {
+        Optional<TodoDTO> todoOptional = todoRepo.findById(id);
+        if (todoOptional.isPresent()) {
+            return new ResponseEntity<>(todoOptional.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Todo not found with id " + id, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public ResponseEntity<?> deleteTodoById(String id) {
+        try {
+            todoRepo.deleteById(id);
+            return new ResponseEntity<>("Successfully deleted with id " + id, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+}
